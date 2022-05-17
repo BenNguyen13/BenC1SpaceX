270e6746955857c938e8bbdb92f5077973eea23b
BenNguyen13 <98521982+BenNguyen13@users.noreply.github.com>
2022-05-17 23:49:05 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+alert("Welcome to the SpaceX console browser");
+
+var food = "70 Days, 10 weeks of rations";
+console.log("Food for the trip (days worth of ration):", food)
+
+var water = "200 litres of water (H2O)";
+console.log("Water for the trip (litres):", water)
+
+var oxygen = "55 metric tons (55,000 Kgs)";
+console.log("Oxygen for the trip (kg, entire journey + more for emergency):", oxygen)
+
+var gforce = "3Gs during lift off, 1G after launch";
+console.log("G-Force experienced (gs):", gforce)
+
+var weight = "155/200 klb";
+console.log("Weight avaliable for storage (klb/klb):", weight)
+
+var fuel = "200 Kgs of fuel";
+console.log("Amount of fuel (more than enough for 1 way, another shuttle will be sent to mars for the return trip):", fuel)
+
+var dtraveled = "11/240 mkm";
+console.log("Distanced travled (over total distance, 240 mkm):", dtraveled)
+
+var speed = "40,000 km/h";
+console.log("Speed of spacecraft (avg speed, km/h):", speed)
+
+
+
