75c99fdab8957c81f496acd474bc3d2496b29955
BenNguyen13 <98521982+BenNguyen13@users.noreply.github.com>
2022-05-18 09:16:46 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,34 +1,34 @@
 alert("Welcome to the SpaceX console browser");
 
-var food = "70 Days, 10 weeks of rations"();
+var food = "70 Days, 10 weeks of rations";
 console.log("Food for the trip (days worth of ration):", food)
 var food = new amountofrations();
 
-var water = "200 litres of water (H2O)"();
+var water = "200 litres of water (H2O)";
 console.log("Water for the trip (litres):", water)
 var water = new litres();
 
-var oxygen = "55 metric tons (55,000 Kgs)"();
+var oxygen = "55 metric tons (55,000 Kgs)";
 console.log("Oxygen for the trip (kg, entire journey + more for emergency):", oxygen)
 var oxygen = new kgs();
 
-var gforce = "3Gs during lift off, 1G after launch"();
+var gforce = "3Gs during lift off, 1G after launch";
 console.log("G-Force experienced (gs):", gforce)
 var gforce = new force();
 
-var weight = "155/200 klb"();
+var weight = "155/200 klb";
 console.log("Weight avaliable for storage (klb/klb):", weight)
 var weight = new avaliableweight();
 
-var fuel = "200 Kgs of fuel"();
+var fuel = "200 Kgs of fuel";
 console.log("Amount of fuel (more than enough for 1 way, another shuttle will be sent to mars for the return trip):", fuel)
 var fuel = new fuelinkgs();
 
-var dtraveled = "11/240 mkm"();
+var dtraveled = "11/240 mkm";
 console.log("Distanced travled (over total distance, 240 mkm):", dtraveled)
 var dtraveled = new traveledovertotal();
 
-var speed = "40,000 km/h"();
+var speed = "40,000 km/h";
 console.log("Speed of spacecraft (avg speed, km/h):", speed)
 var speed = new kmperhour();
 
